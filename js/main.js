734d13d67992da85e1953819379952c4ec350f22
mamartam <127197633+mamartam@users.noreply.github.com>
2026-02-22 14:40:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 const startBtn = document.querySelector(".start-btn");
+const StartBtnAudio = document.querySelector(".StartBtnAudio");
+// ---------------------------------------------------------------------------
 const welcomeSection = document.querySelector(".welcome-section");
 const mainSection = document.querySelector(".main-section");
+// ---------------------------------------------------------------------------
+const questionsSection = document.querySelector(".questions-section");
 const questionHeader = document.querySelector(".question-header");
 const optionItems = document.querySelectorAll(".option-item");
+// ---------------------------------------------------------------------------
 const nextBtn = document.querySelector(".next-btn");
-const activeNextBtn = document.querySelector(".active-next-btn");
-const insideBoxes = document.querySelectorAll(".inside-box");
-const questionsSection = document.querySelector(".questions-section");
 const finishBtn = document.querySelector(".finish-btn");
+// ---------------------------------------------------------------------------
+const insideBoxes = document.querySelectorAll(".inside-box");
+// ---------------------------------------------------------------------------
+const CorrectBtnAudio = document.querySelector(".CorrectBtnAudio");
+const WrongBtnAudio = document.querySelector(".WrongBtnAudio");
+// ---------------------------------------------------------------------------
 const resultSection = document.querySelector(".result-section");
 const userResult = document.querySelector(".user-result");
-
-let count = 0;
-let userAnswers = [];
 
 // Start btn is appearing
 setTimeout(() => {
@@ -21,6 +26,9 @@ setTimeout(() => {
 
 // closing welcome section and showing main section
 startBtn.addEventListener("click", () => {
+  if (StartBtnAudio.paused) {
+    StartBtnAudio.play();
+  }
   welcomeSection.classList.add("pressed-start-btn");
 
   setTimeout(() => {
@@ -29,17 +37,48 @@ startBtn.addEventListener("click", () => {
   }, 2000);
 });
 
+let count = 0;
+let userAnswers = [];
+
+function writeDataToTheDOMElements(index, arrayOfData) {
+  questionsSection.dataset.id = arrayOfData[index].id;
+  questionHeader.textContent = `${index + 1}. ${arrayOfData[index].question}`;
+  arrayOfData[index].answers.forEach((answer, i) => {
+    optionItems[i].textContent = answer;
+  });
+}
+
 async function gettingDataFromExternalSource() {
   const response = await fetch("js/data.json");
   const data = await response.json();
-  console.log(data);
+
   writeDataToTheDOMElements(count, data);
 
-  questionsSection.addEventListener("click", (event) => {
-    let correctOrNot;
-    let idForArray = Number(questionsSection.dataset.id);
-    let correctAnswer = data[idForArray].correct;
+  let len = data.length;
+  nextBtn.addEventListener("click", () => {
+    checkingAnswer(count);
+    count++;
+    writeDataToTheDOMElements(count, data);
 
+    nextBtn.classList.add("hidden");
+    removingOutline();
+  });
+
+  finishBtn.addEventListener("click", () => {
+    checkingAnswer(count);
+
+    setTimeout(() => {
+      mainSection.classList.add("hide-section");
+      mainSection.classList.remove("active");
+      resultSection.classList.add("active");
+      userResults(userAnswers);
+    }, 1000);
+  });
+
+  questionsSection.addEventListener("click", (event) => {
+    let idForArray = Number(event.currentTarget.dataset.id);
+    let correctAnswer = data[idForArray].correct;
+    let correctOrNot;
     if (event.target.classList.contains("option-item")) {
       let userChoice = Number(event.target.dataset.id);
       removingOutline();
@@ -51,73 +90,54 @@ async function gettingDataFromExternalSource() {
         correctOrNot = "not correct";
       }
       userAnswers[idForArray] = correctOrNot;
+
+      if (count + 1 === len) {
+        finishBtn.classList.remove("hidden");
+        nextBtn.classList.add("hidden");
+      } else {
+        nextBtn.classList.remove("hidden");
+      }
     }
-    btnNext(idForArray, data);
   });
 }
 gettingDataFromExternalSource();
 
-// displaying question and answers
-function writeDataToTheDOMElements(number, arrayOfData) {
-  questionsSection.dataset.id = arrayOfData[number].id;
-  questionHeader.textContent = `${number + 1}. ${arrayOfData[number].question}`;
-  for (i in arrayOfData[number].answers) {
-    optionItems[i].textContent = arrayOfData[number].answers[i];
-  }
-}
-// next btn fynction
-function btnNext(idForArray, arrayOfData) {
-  let len = arrayOfData.length;
-  if (idForArray + 1 < len) {
-    nextBtn.classList.remove("hidden");
-  }
-  if (idForArray + 1 === len) {
-    finishBtn.classList.remove("hidden");
-    finishDtn(idForArray, arrayOfData);
-  }
-
-  nextBtn.addEventListener("click", () => {
-    let index = idForArray + 1;
-    removingOutline();
-    writeDataToTheDOMElements(index, arrayOfData);
-    nextBtn.classList.add("hidden");
-    nextBtn.classList.add("hidden");
-    checkingAnswer(idForArray);
+function removingOutline() {
+  optionItems.forEach((element) => {
+    element.classList.remove("chosen-option");
   });
 }
-// finish btn function
-function finishDtn(idForArray, arrayOfData) {
-  finishBtn.addEventListener("click", () => {
-    writeDataToTheDOMElements(idForArray, arrayOfData);
-    checkingAnswer(idForArray);
 
-    setTimeout(() => {
-      mainSection.classList.add("hide-section");
-      mainSection.classList.remove("active");
-      resultSection.classList.add("active");
-      userResults(userAnswers);
-    }, 1000);
-  });
-}
-// function which checks if answer is correct and if so colors the nav box in appropriate color
 function checkingAnswer(idForArray) {
   if (userAnswers[idForArray] === "correct") {
     insideBoxes[idForArray].classList.add("class-with-animation-correct");
+    playCorrectSound();
   } else {
     insideBoxes[idForArray].classList.add("class-with-animation-wrong");
+    playWrongSound();
   }
 }
-// calculate points and display them on the screen
+
+function playCorrectSound() {
+  WrongBtnAudio.pause();
+  WrongBtnAudio.currentTime = 0;
+
+  CorrectBtnAudio.currentTime = 0;
+  CorrectBtnAudio.play();
+}
+
+function playWrongSound() {
+  CorrectBtnAudio.pause();
+  CorrectBtnAudio.currentTime = 0;
+
+  WrongBtnAudio.currentTime = 0;
+  WrongBtnAudio.play();
+}
+
 function userResults(userAnswers) {
   let points = 0;
   userAnswers.forEach((element) => {
     if (element === "correct") points++;
   });
   userResult.textContent = points;
-}
-
-function removingOutline() {
-  optionItems.forEach((element) => {
-    element.classList.remove("chosen-option");
-  });
 }
